f069eae0e267b92425f0c021cf047cef2c6338f6
aurelien.valat <aurelien.valat@A202D3.mshome.net>
2024-10-25 11:44:36 +0200

    classe ConsulterDonnees

--- src/museoflow/modele/ConsulterDonnees.java
+++ src/museoflow/modele/ConsulterDonnees.java
@@ -2,17 +2,39 @@ package museoflow.modele;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ */
 public class ConsulterDonnees {
+	
+	private ArrayList<String> listeConferenciers = new ArrayList<>();
+	private ArrayList<String> listeExpositions = new ArrayList<>();
+	private ArrayList<String> listeVisitesProg = new ArrayList<>();
 
+	/**
+	 * 
+	 * @return la liste des conferenciers
+	 */
 	public ArrayList<String> consulterListeConferencier() {
-		return null;	//Bouchon
+		// Code pour récupérer la liste des conferenciers	
+		return listeConferenciers;	
 	}
 	
+	/**
+	 * 
+	 * @return la liste des expositions
+	 */
 	public ArrayList<String> consulterListeExpositions() {
-		return null;	//Bouchon
+		// Code pour récupérer la liste des expositions
+		return listeExpositions;	
 	}
 	
+	/**
+	 * 
+	 * @return la liste des visites programmées
+	 */
 	public ArrayList<String> consulterVisitesProg() {
-		return null;	//Bouchon
+		// Code pour récupérer la liste des visites
+		return listeVisitesProg;	
 	}
 }
