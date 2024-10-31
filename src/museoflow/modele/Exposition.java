d9aab9b530f4d058aaf5c09cb0004ee0dc478137
Cytron2-0 <153291942+Cytron2-0@users.noreply.github.com>
2024-10-31 20:20:12 +0100

    Corrections Expositions.java + ajout javadoc + changement du délimiteur',' en ';' lors de la lecture des CSV + commencement de la création et affectation des objets Exposition

---
+++ src/museoflow/modele/Exposition.java
@@ -0,0 +1,153 @@
+/*
+ * Exposition.java                                  oct. 2024 
+ * IUT de Rodez Info2 TPD 2024-2025, pas de copyright
+ */
+package museoflow.modele;
+
+/**
+ * Classe objet représentant une exposition
+ * 
+ * @author Aurelien VALAT
+ * @author Cylian POUPIN
+ */
+public class Exposition {
+	
+	private String idExposition;
+
+    private String intituleExposition;
+
+    private String periodeOeuvreDeb;
+
+    private String periodeOeuvreFin;
+
+    private String nombreOeuvre;
+
+    private String[] motsCles;
+
+    private String resume;
+
+    private String dateDebutExpo;
+
+    private String dateFinExpo;
+
+    // Peut-être changer le type de motCles en tableau
+    /**
+     * Constructeur créant une exposition avec les valeurs passées en
+     * paramètres
+     * 
+     * @param idExposition     ID de l'exposition
+     * @param intitule         Intitulé de l'exposition
+     * @param periodeOeuvreDeb Période de début du mouvenemt
+     *                         artistique de l'oeuvre
+     * @param periodeOeuvreFin Période de fin du mouvenemt artistique
+     *                         de l'oeuvre
+     * @param nbOeuvre         Nombre d'oeuvres comprises dans
+     *                         l'exposition
+     * @param motsCles         Mots clés de l'exposition
+     * @param resume           Résumé de l'exposition
+     * @param dateDebutExpo    Date de début de l'exposition
+     * @param dateFinExpo      Date de fin de l'exposition
+     */
+    public Exposition(String idExposition, 
+                      String intitule,
+                      String periodeOeuvreDeb, 
+                      String periodeOeuvreFin, 
+                      String nbOeuvre,
+                      String[] motsCles,
+                      String resume, 
+                      String dateDebutExpo, 
+                      String dateFinExpo) {
+        
+    	this.idExposition = idExposition;
+    	this.intituleExposition = intitule;
+    	this.periodeOeuvreDeb = periodeOeuvreDeb;
+    	this.periodeOeuvreFin = periodeOeuvreFin;
+    	this.nombreOeuvre = nbOeuvre;
+        this.motsCles = motsCles;
+    	this.resume = resume;
+    	this.dateDebutExpo = dateDebutExpo;
+    	this.dateFinExpo = dateFinExpo;
+    }
+
+    /**
+     * Retourne l'ID de l'exposition.
+     * 
+     * @return ID de l'exposition
+     */
+    public String getIdExposition() {
+        return idExposition;
+    }
+
+    /**
+     * Retourne l'intitulé de l'exposition.
+     * 
+     * @return Intitulé de l'exposition
+     */
+    public String getIntituleExposition() {
+        return intituleExposition;
+    }
+
+    /**
+     * Retourne la période de début du mouvement artistique de
+     * l'œuvre.
+     * 
+     * @return Période de début du mouvement artistique de l'œuvre
+     */
+    public String getPeriodeOeuvreDeb() {
+        return periodeOeuvreDeb;
+    }
+
+    /**
+     * Retourne la période de fin du mouvement artistique de l'œuvre.
+     * 
+     * @return Période de fin du mouvement artistique de l'œuvre
+     */
+    public String getPeriodeOeuvreFin() {
+        return periodeOeuvreFin;
+    }
+
+    /**
+     * Retourne le nombre d'œuvres comprises dans l'exposition.
+     * 
+     * @return Nombre d'œuvres comprises dans l'exposition
+     */
+    public String getNombreOeuvre() {
+        return nombreOeuvre;
+    }
+
+    /**
+     * Retourne les mots clés de l'exposition.
+     * 
+     * @return Mots clés de l'exposition
+     */
+    public String[] getMotsCles() {
+        return motsCles;
+    }
+
+    /**
+     * Retourne le résumé de l'exposition.
+     * 
+     * @return Résumé de l'exposition
+     */
+    public String getResume() {
+        return resume;
+    }
+
+    /**
+     * Retourne la date de début de l'exposition.
+     * 
+     * @return Date de début de l'exposition
+     */
+    public String getDateDebutExpo() {
+        return dateDebutExpo;
+    }
+
+    /**
+     * Retourne la date de fin de l'exposition.
+     * 
+     * @return Date de fin de l'exposition
+     */
+    public String getDateFinExpo() {
+        return dateFinExpo;
+    }
+}
--- src/museoflow/modele/GestionFichiers.java
+++ src/museoflow/modele/GestionFichiers.java
@@ -12,8 +12,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderHeaderAware;
+import com.opencsv.CSVReaderHeaderAwareBuilder;
 import com.opencsv.exceptions.CsvException;
 
 /**
@@ -48,7 +50,7 @@ public class GestionFichiers {
      */
     public static void main(String[] args) {
         System.out.println(importerFichier(
-                "C:\\Users\\Utilisateur\\Desktop\\employes 28_08_24 17_26.csv"));
+                "D:\\Cylian\\OneDrive\\Scolaire\\_SAE\\CSVs nettoyés\\expositions 28_08_24 17_26.csv"));
     }
 
     /**
@@ -76,47 +78,76 @@ public class GestionFichiers {
 
         // Création du lecteur CSV
         CSVReader csvReader;
+
+        // Création de l'analyseur CSV pour changer le délimiteur
+        CSVParser csvParser;
+
+        // Instanciation de l'analyseur avec le délimiteur ";"
+        csvParser = new CSVParserBuilder().withSeparator(';').build();
+
+        // Instanciation du lecteur CSV avec le délimiteur ";"
+        csvReader = new CSVReaderHeaderAwareBuilder(reader)
+                .withCSVParser(csvParser).build();
+
         try {
-            csvReader = new CSVReaderHeaderAware(reader);
+            // Essai de la lecture complète du CSV
+            List<String[]> csvLu = new ArrayList<>();
+            csvLu = csvReader.readAll();
 
-            try {
-                // Essai de la lecture complète du CSV
-
-                // DEBUG --------------------------------------
-                List<String[]> testCSVReader = new ArrayList<>();
-                testCSVReader = csvReader.readAll();
-
-                for (String[] colonne : testCSVReader) {
-                    System.out.println(Arrays.toString(colonne));
-                }
-
-                System.out.println((testCSVReader.get(1))[1]);
-
-                // éléments déja découpés dans les tableaux de
-                // string dans l'arraylist -> lecture et préparation
-                // faite en même temps
-                // TODO l'affectation dans les objets (for automatique
-                // qui crée les objets)
-
-                // DEBUG ---------------------------------------
-                csvReader.close();
-                return true;
-
-                // Eventuelles erreurs de lecture du CSV
-            } catch (IOException e) {
-                System.out.println("Le CSV a pu être ouvert, "
-                        + "mais une erreur est survenue durant la lecture.\n"
-                        + e);
-
-            } catch (CsvException e) {
-                System.out.println("Le CSV a pu être ouvert, "
-                        + "mais un validateur est défaillant\n" + e);
+            // DEBUG --------------------------------------------
+            for (String[] colonne : csvLu) {
+                System.out.println(Arrays.toString(colonne));
             }
 
+            System.out.println((csvLu.get(0))[5]);
+
+            // essai affectation objets avec les expositions
+            System.out.println("\n\n -----------");
+
+            for (String colonne : csvLu.get(0)) {
+                System.out.println(colonne);
+            }
+            // TODO conversion String mots clés en String[] sur les
+            // virgules (possible avec opencsv en lui passant le
+            // string ?)
+            String[] tableauTest = { "jaaj", "jaaj_2" };
+
+            // Création des objets expositions avec les attributs lus
+            // du CSV
+            // TODO tableau d'expositions de taille le nombre de
+            // lignes du CSV (voir screen)
+            Exposition exp1 = new Exposition(
+                    ((csvLu.get(0))[0]),
+                    ((csvLu.get(0))[1]),
+                    ((csvLu.get(0))[2]),
+                    ((csvLu.get(0))[3]),
+                    ((csvLu.get(0))[4]),
+                    tableauTest,
+                    ((csvLu.get(0))[5]),
+                    ((csvLu.get(0))[6]),
+                    ((csvLu.get(0))[7]));
+            
+            System.out.println("ID de l'expo : " + exp1.getIdExposition());
+            // DEBUG --------------------------------------------
+
+            // éléments déja découpés dans les tableaux de
+            // string dans l'arraylist -> lecture et préparation
+            // faite en même temps
+            // TODO l'affectation dans les objets (for automatique
+            // qui crée les objets)
+
+
+            csvReader.close();
+            return true;
+
+            // Eventuelles erreurs de lecture du CSV
         } catch (IOException e) {
-            System.out.println(
-                    "Erreur lors de la lecture des en-têtes de colonnes.\n"
-                            + e);
+            System.out.println("Le CSV a pu être ouvert, "
+                    + "mais une erreur est survenue durant la lecture.\n" + e);
+
+        } catch (CsvException e) {
+            System.out.println("Le CSV a pu être ouvert, "
+                    + "mais un validateur est défaillant\n" + e);
         }
         return false;
     }
